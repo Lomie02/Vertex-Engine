adf60c05ec1bbb94027e87d291cd7bfea056b4c5
Lomie02 <dylansmith1844@gmail.com>
2022-12-17 22:34:32 +1100

    stb Upload

--- Vertex Engine/SceneManager.cpp
+++ Vertex Engine/SceneManager.cpp
@@ -13,6 +13,7 @@ void SceneManager::SetUpWindow(GLFWwindow* _window)
 	for (int i = 0; i < m_SceneList.size(); i++)
 	{
 		m_SceneList.at(i)->GiveWindow(_window);
+		m_SceneList.at(i)->GiveSceneManager(this);
 	}
 }
 
--- Vertex Engine/VertexScene.h
+++ Vertex Engine/VertexScene.h
@@ -15,12 +15,12 @@ public:
 
 	virtual void Rendering(Vertex2D* render) = 0;
 
-	virtual void GiveWindow(GLFWwindow* window) { m_Window = window; }
-	virtual void GiveSceneManager(SceneManager* scenes) { m_SceneManager = scenes; }
+	void GiveWindow(GLFWwindow* window) { m_Window = window; }
+	void GiveSceneManager(SceneManager* scenes) { m_SceneManager = scenes; }
 	virtual AssetManager GetAssets() { return m_Manager; }
 	char m_SceneName[15] = "MyScene";
 protected:
 	AssetManager m_Manager;
 	SceneManager* m_SceneManager;
-	GLFWwindow m_Window;
+	GLFWwindow* m_Window;
 };
